1bfb9d74ce5b27366ca5b202549ed623b60415fd
Artem Bogdanov <artyomeg@mail.ru>
2023-11-11 03:06:41 +0300

    v3.4

--- src/views/layout/RootLayout.tsx
+++ src/views/layout/RootLayout.tsx
@@ -3,17 +3,103 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { LocaleProvider } from '@application/providers/LocaleProvider';
 import { DEFAULT_LOCALE } from '@shared/lib/types/common';
+import styles from '../../../styles/Home.module.css';
+import LHeader from './LHeader';
 
 export default function RootLayout({ locale = DEFAULT_LOCALE, children }) {
   const defaultTheme = createTheme();
 
   return (
     <>
-      <LocaleProvider locale={locale}>
-        <ThemeProvider theme={defaultTheme}>
-          {children}
-        </ThemeProvider>
-      </LocaleProvider>
+      <div className={styles.container}>
+        <LHeader />
+        <LocaleProvider locale={locale}>
+          <ThemeProvider theme={defaultTheme}>
+            <main>
+            {children}
+            </main>
+          </ThemeProvider>
+        </LocaleProvider>
+
+        <footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          </a>
+        </footer>
+
+      </div>
+
+      <style jsx>{`
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family:
+              -apple-system,
+              BlinkMacSystemFont,
+              Segoe UI,
+              Roboto,
+              Oxygen,
+              Ubuntu,
+              Cantarell,
+              Fira Sans,
+              Droid Sans,
+              Helvetica Neue,
+              sans-serif;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
     </>
   );
 }
