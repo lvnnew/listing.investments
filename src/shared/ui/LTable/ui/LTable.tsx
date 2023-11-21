3ee1fcb4537f4affd3b1217c4956efcb3a43c53c
Artem Bogdanov <artyomeg@mail.ru>
2023-11-21 17:11:28 +0300

    v1.1.1

--- src/shared/ui/LTable/ui/LTable.tsx
+++ src/shared/ui/LTable/ui/LTable.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,25 +7,6 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 import { i18n } from "next-i18next";
-// import { LocaleContext } from '@application/providers/LocaleProvider/lib/LocaleContext';
-
-function createData(
-  name,
-  calories,
-  fat,
-  carbs,
-  protein,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 export function LTable({ columns, lines }) {
   const getShownValue = ({ line, fn }) => (
--- src/views/index/ui/IndexTable.tsx
+++ src/views/index/ui/IndexTable.tsx
@@ -1,35 +1,8 @@
 import React, { useContext } from "react";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import { i18n, useTranslation } from 'next-i18next';
-// import { LocaleContext } from '@application/providers/LocaleProvider/lib/LocaleContext';
 
-import { easyInvestPlatformsEntity } from '../../../entities/easyInvestPlatforms';
-import { LTable } from '../../../shared/ui/LTable';
+import { LTable } from '@shared/ui/LTable';
 import { easyInvestPlatformsLines } from '@views/index/lib/easyInvestPlatformsLines';
-
-function createData(
-  name,
-  calories,
-  fat,
-  carbs,
-  protein,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
 
 export default function IndexTable() {
   const { t } = useTranslation('easy-investing');
@@ -38,9 +11,7 @@ export default function IndexTable() {
     {
       fieldKey: 'ID',
       fieldLabel: '#',
-      valueMaker: (line, lineIndex) => (<>
-        {lineIndex + 1}
-      </>)
+      valueMaker: (line, lineIndex) => (<>{lineIndex + 1}</>)
     },
     {
       fieldKey: 'TITLE',
