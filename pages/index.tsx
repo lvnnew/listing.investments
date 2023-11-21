3c0617864930fe1ba53195ab6d0366bb87d55bf4
Artem Bogdanov <artyomeg@mail.ru>
2023-11-21 14:09:04 +0300

    v1.0.1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,15 @@
 import Head from 'next/head';
-import { useEffect } from 'react';
-import { useTranslation } from "next-i18next";
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import RootLayout from '@views/layout/RootLayout';
 import { IndexTable } from '@views/index';
-import { getLang } from '@shared/lib/helpers/getLang';
 
 type Props = {
   // Add custom props here
 }
 
 const Index = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  // const { i18n, t } = useTranslation("common");
-  const { t, i18n } = useTranslation('common')
-
-  console.log('getLang', getLang());
-  console.log('test', t("msg_003"));
-
-
-  // const lang = req.query.lang as string;
-  console.log('i18n-before', i18n);
-
-  useEffect(() => {
-    console.log('i18n-after', i18n);
-  });
-
   return (
     <>
       <Head>
@@ -34,7 +17,6 @@ const Index = (_props: InferGetStaticPropsType<typeof getStaticProps>) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <RootLayout>
-        {/*{i18n}*/}
         <IndexTable />
       </RootLayout>
     </>
